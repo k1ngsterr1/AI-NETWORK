7c736215bea53cca1e5aa9e2ff650a91e4e41416
Ruslan <ruslanmakhmatov@gmail.com>
2022-07-21 00:44:23 +0600

    alpha 0.8

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "../styles/about_styles/about-styles.css";
 
 const AboutUs = () => {
   return (
     <div className="second-screen">
       <h2 className="about-project-header">About Project</h2>
+      <figure className="separator"></figure>
+      <div className="glass-div">
+        <p className="about-us-paragraph">
+          This Project Was Created In Order To Show the basics of the neural
+          networks and machine learning, through the chatting way. To put it
+          more simply, you can create{" "}
+          <span className="blue-txt">your own AI</span> and train it just with
+          chatting
+        </p>
+      </div>
+      <button className="read-more">READ MORE</button>
     </div>
   );
 };
