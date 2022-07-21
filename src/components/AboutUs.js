0a1a52c32bceda3cc0834683069792ded2d9189f
Ruslan <ruslanmakhmatov@gmail.com>
2022-07-21 18:13:54 +0600

    alpha 0.9

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../styles/about_styles/about-styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
 
 const AboutUs = () => {
   return (
     <div className="second-screen">
       <h2 className="about-project-header">About Project</h2>
-      <figure className="separator"></figure>
+      <figure className="separator-2"></figure>
       <div className="glass-div">
         <p className="about-us-paragraph">
           This Project Was Created In Order To Show the basics of the neural
@@ -16,6 +20,38 @@ const AboutUs = () => {
         </p>
       </div>
       <button className="read-more">READ MORE</button>
+      <section className="cards-container">
+        <article className="card-one">
+          <h3 className="card-one-header">
+            <FontAwesomeIcon className="icon-1" icon={faBrain} /> STEP 1
+          </h3>
+          <figure className="separator-c-1"></figure>
+          <p className="card-one-text">
+            first of all, you should create and name your aI and then make only
+            then make a chat
+          </p>
+        </article>
+        <article className="card-two">
+          <h3 className="card-two-header">
+            <FontAwesomeIcon className="icon-2" icon={faRobot} /> STEP 2
+          </h3>
+          <figure className="separator-c-2"></figure>
+          <p className="card-two-text">
+            Now you can train Your AI, Just chatting with it. the more you
+            chatting - the smarter aI Is
+          </p>
+        </article>
+        <article className="card-three">
+          <h3 className="card-three-header">
+            <FontAwesomeIcon className="icon-3" icon={faUsers} /> STEP 3
+          </h3>
+          <figure className="separator-c-3"></figure>
+          <p className="card-three-text">
+            Share your own neural network with friends and other users, have fun
+            and enjoy it!
+          </p>
+        </article>
+      </section>
     </div>
   );
 };
